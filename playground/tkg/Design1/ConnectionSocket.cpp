741a3da31cb7858fca31addf90aa7c4be319a1a5
Takumi Hara <69781798+tacomeet@users.noreply.github.com>
2023-05-17 20:33:00 +1000

    chore: rm unneeded comment out

--- playground/tkg/Design1/ConnectionSocket.cpp
+++ playground/tkg/Design1/ConnectionSocket.cpp
@@ -15,7 +15,6 @@
 #include <map>
 #include <stdexcept>
 
-// #include "EventManager.hpp"
 #include "HttpRequest.hpp"
 #include "HttpResponse.hpp"
 
@@ -27,36 +26,6 @@ void ConnectionSocket::handle_response(EventManager &event_manager) {
   request_.refresh();
   std::cout << "response: " << response_.raw_data_ << std::endl;
   response_.sendResponse(event_manager);
-  // send_response(event_manager);
 }
 
 void ConnectionSocket::handle_request(EventManager &event_manager) { request_.readRequest(event_manager); }
-
-// void ConnectionSocket::send_response(EventManager &event_manager) {
-//   const char *response = response_.c_str();
-//   std::cout << "sending response \n";
-//   int size = response_size_ - sending_response_size_;
-//   if (size > kWriteSize) {
-//     size = kWriteSize;
-//   }
-//   int res = sendto(fd_, &response[sending_response_size_], size, 0, NULL, 0);
-//   if (res == -1) {
-//     perror("sendto");
-//     throw std::runtime_error("send error");
-//   }
-//   std::string res_str = std::string(response);
-//   std::cout << "response sent: "
-//             << "'" << res_str.substr(sending_response_size_, size) << "'"
-//             << " (size:" << res << ")" << std::endl;
-//   sending_response_size_ += size;
-//   if (sending_response_size_ == response_size_) {
-//     setToReadingState(event_manager);
-//   }
-// }
-
-// void ConnectionSocket::setToReadingState(EventManager &em) {
-//   sending_response_size_ = 0;
-//   em.addChangedEvents((struct kevent){fd_, EVFILT_WRITE, EV_DISABLE, 0, 0, 0});
-//   em.addChangedEvents((struct kevent){fd_, EVFILT_READ, EV_ENABLE, 0, 0, 0});
-//   state_ = kSocFree;
-// }
