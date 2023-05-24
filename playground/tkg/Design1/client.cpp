9c4f7c60c967d1ebbb620f36372d70edeb2674c8
Takumi Hara <69781798+takumihara@users.noreply.github.com>
2023-05-24 15:16:42 +1000

    Headerのparseをより厳密に！ (#18)

--- playground/tkg/Design1/client.cpp
+++ playground/tkg/Design1/client.cpp
@@ -105,17 +105,16 @@ std::string getRequest(const std::string &arg) {
   if (arg == "chunked") {
     request += "POST /a.cgi HTTP/1.1\r\n";
     request += "Host: localhost\r\n";
-    request += "Transfer-Encoding: chunked\r\n ";
-    request += "Content-Type: text/plain\r\n";
+    request += "Transfer-Encoding: chunked     ,  chunked,     chunked\r\n ";
     request += "\r\n";
     request += "4\r\nWiki\r\n7\r\npedia i\r\nB\r\nn \r\nchunks.\r\n0\r\n\r\n";
     request += "\r\n";
   } else {
-    request += "POST /a.cgi HTTP/1.1\r\n";
+    request += "POST /a.cgi?query HTTP/1.1\r\n";
     // request += "POST /index.html HTTP/1.1\r\n";
     request += "Host: localhost\r\n";
-    request += "Content-Type: text/plain\r\n";
     request += "Content-Length: 5\r\n";
+    request += "Date: Wed, 16 Oct 2019 07:28:00 GMT\r\n";
     request += "\r\n";
     request += arg;
     request += "\r\n";
