becf051aa3c0555a000188bac88f20f4e45536fb
deer <deersaga4@gmail.com>
2023-06-26 13:32:49 +0900

    fix: remove *.conf and add *.template, modified configTranslater.sh

--- playground/tkg/Design1/configTranslater.sh
+++ playground/tkg/Design1/configTranslater.sh
@@ -7,10 +7,12 @@ escaped_cur_dir=$(echo "$cur_dir" | sed 's/\//\\\//g')
 echo "$escaped_cur_dir"
 
 # ディレクトリ内のファイルをイテレート
-for file_path in "$directory_path"/*; do
+for file_path in "$directory_path"/*.template; do
     if [ -f "$file_path" ]; then
-        sed "s/pwd/${escaped_cur_dir}/g" "$file_path" > "tmp.txt"
-		cat "tmp.txt" > "$file_path"
+        new_file="${file_path%.*}.conf"
+        cp "$file_path" "$new_file"
+        sed "s/pwd/${escaped_cur_dir}/g" "$new_file" > "tmp.txt"
+		cat "tmp.txt" > "$new_file"
     fi
 done
 rm tmp.txt
