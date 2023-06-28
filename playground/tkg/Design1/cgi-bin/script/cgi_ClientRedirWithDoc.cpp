f8b9791005bd9367061d880113f4487498863773
deer <deersaga4@gmail.com>
2023-06-29 02:43:34 +0900

    fix: CGI Response handling

--- playground/tkg/Design1/cgi-bin/script/cgi_ClientRedirWithDoc.cpp
+++ playground/tkg/Design1/cgi-bin/script/cgi_ClientRedirWithDoc.cpp
@@ -6,8 +6,8 @@
 int main() {
   std::string input;
   std::cin >> input;
-  std::cout << "Location:http://example.com" << std::endl;
   std::cout << "Status:301 CRWD(CGI)" << std::endl;
+  std::cout << "Location:http://example.com" << std::endl;
   std::cout << "Content-Type:text/html" << std::endl;
   std::cout << std::endl;
   std::stringstream ss;
