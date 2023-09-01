c7299f112f99100991510fc7eb012d99f527abb2
Takumi Hara <takumihara1226@gmail.com>
2023-09-01 14:47:53 +0900

    feat: stress test

---
+++ stress_test.js
@@ -0,0 +1,22 @@
+import { check } from "k6";
+import http from "k6/http";
+
+export const options = {
+  thresholds: {
+    http_req_failed: ["rate<0.01"],
+    http_req_duration: ["p(90)<2000"],
+  },
+};
+
+const url = "http://localhost/html";
+
+export default function () {
+  const headers = {
+    "Content-Type": "application/json",
+  };
+
+  const res = http.get(url);
+  check(res, {
+    is_status_200: (r) => r.status === 200,
+  });
+}
