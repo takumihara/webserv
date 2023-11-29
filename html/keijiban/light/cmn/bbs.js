c24bbdffc4f747cc0ac47f2cb4d00ee304279133
Takumi Hara <takumihara1226@gmail.com>
2023-11-28 22:27:08 -0800

    delete perl code

--- html/keijiban/light/cmn/bbs.js
+++
@@ -1,7 +0,0 @@
-function regform() {
-    if (document.getElementById("reg-tbl").style.display == "") {
-		document.getElementById("reg-tbl").style.display = "none";
-    } else {
-		document.getElementById("reg-tbl").style.display = "";
-    }
-}
